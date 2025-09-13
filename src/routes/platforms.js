18523fad080df1c4c1650c690d5ab3c4c70a1c5c
chadskiw <146681598+chadskiw@users.noreply.github.com>
2025-09-13 18:10:08 -0400

    Add files via upload

--- src/routes/platforms.js
+++ src/routes/platforms.js
@@ -14,8 +14,8 @@ router.get('/__alive', (_req, res) =>
 
 // ✅ Mount platform routers (Express routers, not adapters)
 router.use('/espn', require('../../routers/espnRouter'));
-router.use('/sleeper', require('../../routers/routers/sleeperRouter'));
-router.use('/health',  require('../../routers/routers/healthRouter'));
+router.use('/sleeper', require('../../routers/sleeperRouter'));
+router.use('/health',  require('../../routers/healthRouter'));
 
 // Optional: quick list of mounts
 router.get('/__routes', (_req, res) => {
