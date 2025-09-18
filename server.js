4eba53acd963e64feb9d30713c9d98cfe3afc343
chadskiw <146681598+chadskiw@users.noreply.github.com>
2025-09-17 21:06:03 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -54,10 +54,6 @@ app.use('/api/fein-auth', feinAuthRouter);
 // server.js
 app.use('/api/auth', require(authRouter));
 
-// server.js
-const path = require('path');
-const express = require('express');
-
 
             // same-origin cookie endpoints + meta upsert
 app.use('/api/platforms/espn', requireEspnHeaders, espnRouter);
