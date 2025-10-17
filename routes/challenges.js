ba27ed2844b5ac87be87cc6096470f7392aad926
chadskiw <146681598+chadskiw@users.noreply.github.com>
2025-10-17 11:44:16 -0400

    Add files via upload

--- routes/challenges.js
+++ routes/challenges.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const crypto = require('crypto');
-const cookie = require('cookie-parser');
+const cookie = require('cookie');
 
 const router = express.Router();
 const pool = new Pool({
