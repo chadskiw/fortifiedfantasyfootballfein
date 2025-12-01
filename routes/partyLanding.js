a342924abc6335ce8b196470967ac103ff362aaa
chadskiw <146681598+chadskiw@users.noreply.github.com>
2025-11-30 19:51:44 -0500

    Add files via upload

--- routes/partyLanding.js
+++ routes/partyLanding.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.get('/p/:partyId', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'trashtalk', 'p.html'));
 });
-
-router.get('/p', (req, res) => {
-  if (req.query?.party_id) {
-    return res.sendFile(path.join(__dirname, '..', 'public', 'trashtalk', 'p.html'));
-  }
+// in routes/partyLanding.js or similar
+router.get('/p', requireIdentity, (req, res) => {
+    if (req.query?.party_id) {
+  res.sendFile(path.join(__dirname, '..', 'public', 'trashtalk', 'p.html'));
+    }
   return res.redirect('/');
 });
 
