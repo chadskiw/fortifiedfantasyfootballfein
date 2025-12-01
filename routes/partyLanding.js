ee8fedf85c4e8ea229ac01884b6ee95059b91837
chadskiw <146681598+chadskiw@users.noreply.github.com>
2025-11-30 20:00:18 -0500

    Add files via upload

--- routes/partyLanding.js
+++ routes/partyLanding.js
@@ -1,9 +1,14 @@
 // routes/partyLanding.js
 const express = require('express');
 const path = require('path');
+const { getCurrentIdentity } = require('../services/identity');
 
 const router = express.Router();
-
+async function requireIdentity(req, res) {
+  const me = await getCurrentIdentity(req, pool);
+if (!me) return res.status(401).json({ error: 'Not logged in' });
+  return me;
+}
 router.get('/p/:partyId', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'trashtalk', 'p.html'));
 });
